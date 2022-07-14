e95d373103313c1b76d84649fde9468e37288729
MaratMM <woodi94@mail.ru>
2022-07-14 22:13:40 +0300

    разметили header без мобилки

---
+++ #src/js/script.js
@@ -0,0 +1,27 @@
+//добавление других js  к общему файлу
+
+
+//необходимая часть , не удалять!!!!!!!!!!
+// function testWebP(callback) {
+//     var webP = new Image();
+//     webP.onload = webP.onerror = function () {
+//         callback(webP.height == 2);
+//     };
+//     webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+// }
+// testWebP(function (support) {
+//     if (support == true) {
+//         document.querySelector('body').classList.add('webp');
+//     } else {
+//         document.querySelector('body').classList.add('no-webp');
+//     }
+// });
+
+//основной скрипт
+
+window.addEventListener('DOMContentLoaded', function () {
+    //////////////функция вывода размеров   console.log(salePrice[i].getBoundingClientRect().width);
+
+});
+
+
