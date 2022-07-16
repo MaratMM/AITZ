06ceb44289055bdd757cf435d2815850c4f69def
MaratMM <woodi94@mail.ru>
2022-07-16 19:37:16 +0300

    окно поиска, выпадающее меню

--- dist/js/script.js
+++ dist/js/script.js
@@ -21,18 +21,43 @@
 
 window.addEventListener('DOMContentLoaded', function () {
     //////////////функция вывода размеров   console.log(salePrice[i].getBoundingClientRect().width);
-/////автоматический маргин-топ для main//////////////
-window.addEventListener("resize", ()=> {
-    document.querySelector('main').style.marginTop = document.querySelector('header').getBoundingClientRect().height + 20 + 'px';
-});
-    
+    /////автоматический маргин-топ для main и скролл для наверх для правильного отображения//////////////
+    const marginTop = () => {
+        document.querySelector('main').style.marginTop = document.querySelector('header').getBoundingClientRect().height + 20 + 'px';
+    }
+    marginTop();
+    window.scrollTo({
+        top: 0,
+        left: 0
+    });
+    window.addEventListener("resize", marginTop);
+
 
     //////кнопка меню бургера//////////////
-    document.querySelector('.open-close-menu-button__item').addEventListener("click", () => {
+    document.querySelector('.open-close-menu-button__item')?.addEventListener("click", () => {
         document.querySelector('.open-close-menu-button__item').classList.toggle('active');
         document.querySelector('.header-navigation').classList.toggle('active');
         document.querySelector('body').classList.toggle('active');
     });
+
+    //////////////смена языка RU и EN///////////////////
+    for (let i = 0; i < document.querySelectorAll('.header-main__language-item').length; i++) {
+        document.querySelectorAll('.header-main__language-item')[i].addEventListener("click", () => {
+            if (document.querySelectorAll('.header-main__language-item')[i].classList.contains('active')) {} else {
+                for (let k = 0; k < document.querySelectorAll('.header-main__language-item').length; k++) {
+                    document.querySelectorAll('.header-main__language-item')[k].classList.toggle('active');
+                }
+            }
+        })
+    }
+
+    ////////////////////////////////////
+    this.document.querySelector('.header-main__icon_search')?.addEventListener('click', ()=> {
+        this.document.querySelector('.header-main__icon_search').classList.toggle('active');
+        this.document.querySelector('.header-main__input-search').classList.toggle('active');
+        this.document.querySelector('.popravka-input').classList.toggle('active');
+    })
+
 });
 
 
